bdfd2438866fdbed7a760e21e1a96e296bbadfe0
Atef Ben Ali <atef.bettaib@gmail.com>
2024-08-26 12:24:01 +0100

    build: add `ionic/capacitor` config files

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from "@capacitor/cli";
+
+const config: CapacitorConfig = {
+  appId: "com.todo.app.lazy",
+  appName: "Todom",
+  webDir: "build",
+};
+
+export default config;
